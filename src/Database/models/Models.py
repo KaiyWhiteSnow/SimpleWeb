accac4c62fbd9889c8634043959b07d040ae7bd1
KaiyWhiteSnow <106684837+KaiyWhiteSnow@users.noreply.github.com>
2023-11-21 16:16:40 +0100

    Added some basic things to get started

---
+++ src/Database/models/Models.py
@@ -0,0 +1,5 @@
+from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
--- src/Main.py
+++ src/Main.py
@@ -1,1 +1,14 @@
-# TODO: Make this into init file to load modules
+from flask import Flask
+
+# Main instance of Flask app
+app = Flask(__name__)
+
+# Blueprint imports
+from auth import auth
+
+# Registering blueprints
+app.register_blueprint(auth, url_prefix="/auth")
+app.register_blueprint(user, url_prefix="/User")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5500)
